38fbd1b55e6ad91e933798d46eac0b2c839258ff
Pedro Henrique Knebel <93292374+pknebel@users.noreply.github.com>
2025-08-09 17:46:22 -0300

    initial commit

---
+++ Jpars/src/main/java/br/com/unochapeco/pedro/Jpars/JparsApplication.java
@@ -0,0 +1,13 @@
+package br.com.unochapeco.pedro.Jpars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparsApplication.class, args);
+	}
+
+}
