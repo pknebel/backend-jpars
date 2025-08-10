50649f66f4a955fad9bfec4bca063bfe41b0a934
Pedro Henrique Knebel <93292374+pknebel@users.noreply.github.com>
2025-08-09 21:40:40 -0300

    teste

--- Jpars/src/main/java/br/edu/unochapeco/jpars/JparsApplication.java
+++ Jpars/src/main/java/br/edu/unochapeco/jpars/JparsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JparsApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(JparsApplication.class, args);
 	}
 
